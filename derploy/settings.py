3e126b1cef543332ec3b0e89aada407a0b3a2d6e
Dmitry Oleynik <johnkeats2k11@gmail.com>
2018-07-26 13:07:36 +0300

    Setting typo fix

--- derploy/settings.py
+++ derploy/settings.py
@@ -75,7 +75,7 @@ WSGI_APPLICATION = 'derploy.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_spycopg2',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': '',
         'USER': '',
         'PASSWORD': 'yoursecretpassword',
